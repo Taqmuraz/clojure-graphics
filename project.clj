a52adb93f131c180952046037a79001d9e2e8890
Taqmuraz <lulook23@gmail.com>
2024-04-25 17:25:20 +0200

    rotating triangle example

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject rotating-triangle "1.0"
+  :description "Rotating triangle example"
+  :dependencies [[org.clojure/clojure "1.10.2"]
+                 [quil "2.8.0"]]
+  :main ^:skip-aot rotating-triangle.core)
+
