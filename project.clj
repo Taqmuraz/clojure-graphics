32358155c32fac69c2cc5c1029fad0d4eb65dc37
Taqmuraz <lulook23@gmail.com>
2024-04-26 20:35:26 +0200

    no license

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
 (defproject window "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Clojure graphics experience"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [quil "4.3.1323"]]
   :main ^:skip-aot window.core)
