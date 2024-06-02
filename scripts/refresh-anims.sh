435332a8fae3b082343a8b9f82919d4d6dcafeae
Taqmuraz <lulook23@gmail.com>
2024-06-02 12:35:50 +0200

    scripts update

--- scripts/refresh-anims.sh
+++ scripts/refresh-anims.sh
@@ -1,3 +1,3 @@
 sh make-anim.sh ../../Blender/Human/Human_LongSword_Attack_Forward.blend ../res/knight_attack.png
 sh make-anim.sh ../../Blender/Human/Human_LongSword_Walk.blend ../res/knight_walk.png
-sh make-anim.sh ../../Blender/Human/Human_LongSword_Defeated.blend ../res/knight_dead.png
+sh make-anim.sh ../../Blender/Human/HumanDefeated.blend ../res/knight_dead.png
