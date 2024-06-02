772a54f970801848947026bef6a92154242f82df
Taqmuraz <lulook23@gmail.com>
2024-06-02 12:50:05 +0200

    updated camera

--- scripts/refresh-anims.sh
+++ scripts/refresh-anims.sh
@@ -1,3 +1,4 @@
 sh make-anim.sh ../../Blender/Human/Human_LongSword_Attack_Forward.blend ../res/knight_attack.png
 sh make-anim.sh ../../Blender/Human/Human_LongSword_Walk.blend ../res/knight_walk.png
+sh make-anim.sh ../../Blender/Human/Human_LongSword_Idle.blend ../res/knight_idle.png
 sh make-anim.sh ../../Blender/Human/HumanDefeated.blend ../res/knight_dead.png
